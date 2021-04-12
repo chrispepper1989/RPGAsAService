5dd062ce004ff568cc914bbc7aa89575c3228eab
chris pepper <chris.pepper@dunnhumby.com>
2021-04-12 13:18:21 +0100

    Initial battle system

---
+++ RPGAAS/RPGAAS/BattleSystem.cs
@@ -0,0 +1,7 @@
+﻿namespace RPGAAS
+{
+    public class BattleSystem
+    {
+        
+    }
+}
---
+++ RPGAAS/RPGAAS/BattleSystemTest.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace RPGAAS
+{
+    public class BattleSystemTest
+    {
+        [Fact]
+        void When_CharacterAttacksOther_HealthIsTaken()
+        {
+            // arrange
+            /*
+             * |Char | health | attack power |
+             * |John | 50     | 5           |
+             * |Ogre | 30     | 6           |
+             */
+            
+            // act
+            // john attack ogre once
+            
+            // assert
+            // I Expect
+            /*
+             * |Char | health | attack power |
+             * |John | 50     | 5           |
+             * |Ogre | 25     | 6           |
+             */
+            
+            
+        }
+        
+        
+    }
+}
