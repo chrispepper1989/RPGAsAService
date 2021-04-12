f1d07dfd88a6433869d60ab3eb9eed2ecca803b0
chris pepper <chris.pepper@dunnhumby.com>
2021-04-12 16:03:25 +0100

    adding tests for idea of trigger buffs (in this case attack is increased when character health is low)

--- RPGAAS/RPGAAS/BattleSystemTest.cs
+++ RPGAAS/RPGAAS/BattleSystemTest.cs
@@ -140,6 +140,41 @@ namespace RPGAAS
         }
         
         [Fact]
+        void When_CharacterIsAtLowHealth_TriggersSpecialModifier()
+        {
+            // arrange
+            Dictionary<string, int> characterAttackPower = new Dictionary<string, int>
+            {
+                {"Hero", 10},
+                {"Ogre", 6}
+            };
+            Dictionary<string, int> characterHealth = new Dictionary<string, int>
+            {
+                {"Hero", 1},
+                {"Ogre", 50}
+            };
+            
+            // act
+            ModifierRepository modifierRepository = new ModifierRepository();
+
+            BattleSystem battleSystem = new BattleSystem(characterAttackPower, characterHealth, modifierRepository);
+            //todo this would be a nice way to have it
+            //modifierRepository.AddModifier("Hero", new WhenHealthBelow(5).IncreaseAttackMultiplier(2));
+            battleSystem.Attack("Hero", "Ogre");
+            
+       
+            // assert
+            // I Expect special multiplier to apply
+            battleSystem.GetHealth("Ogre").Should().Be(30);
+            battleSystem.GetHealth("Hero").Should().Be(1);
+            battleSystem.IsDead("Ogre").Should().Be(false);
+            battleSystem.IsDead("Hero").Should().Be(false);
+        }
+        
+        
+        
+        [Fact]
+        //todo this is now a modifier repo test really
         void When_CharacterHasBuffMultipliersAndIncrease_IncreaseHappensBeforeMultiplier()
         {
             // arrange
@@ -173,4 +208,6 @@ namespace RPGAAS
             battleSystem.IsDead("Hero").Should().Be(false);
         }
     }
+
+
 }
