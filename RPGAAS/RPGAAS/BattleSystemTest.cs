92fa6c589b659e2220add732d374a99b67338568
chris pepper <chris.pepper@dunnhumby.com>
2021-04-12 13:36:37 +0100

    second test passing with simple implementation

--- RPGAAS/RPGAAS/BattleSystemTest.cs
+++ RPGAAS/RPGAAS/BattleSystemTest.cs
@@ -39,7 +39,52 @@ namespace RPGAAS
              */
             battleSystem.GetHealth("Ogre").Should().Be(25);
             battleSystem.GetHealth("Hero").Should().Be(50);
+            battleSystem.IsDead("Ogre").Should().Be(false);
             battleSystem.IsDead("Hero").Should().Be(false);
+        }
+        
+        [Fact]
+        void When_CharacterAttacksOtherUntilHealthIsGone_CharacerIsDead()
+        {
+            // arrange
+            /*
+             * |Char | health | attack power |
+             * |Hero | 50     | 5           |
+             * |Ogre | 30     | 6           |
+             */
+            Dictionary<string, int> characterAttackPower = new Dictionary<string, int>
+            {
+                {"Hero", 10},
+                {"Ogre", 6}
+            };
+            Dictionary<string, int> characterHealth = new Dictionary<string, int>
+            {
+                {"Hero", 50},
+                {"Ogre", 30}
+            };
+            
+            // act
+            // john attack ogre once
+            BattleSystem battleSystem = new BattleSystem(characterAttackPower, characterHealth);
+            
+            
+            battleSystem.Attack("Hero", "Ogre");
+            battleSystem.GetHealth("Ogre").Should().Be(20);
+            
+            battleSystem.Attack("Hero", "Ogre");
+            battleSystem.GetHealth("Ogre").Should().Be(10);
+            
+            battleSystem.Attack("Hero", "Ogre");
+            // assert
+            // I Expect
+            /*
+             * |Char | health | attack power |
+             * |Hero | 50     | 5           |
+             * |Ogre | 25     | 6           |
+             */
+            battleSystem.GetHealth("Ogre").Should().Be(0);
+            battleSystem.GetHealth("Hero").Should().Be(50);
+            battleSystem.IsDead("Ogre").Should().Be(true);
             battleSystem.IsDead("Hero").Should().Be(false);
         }
         
